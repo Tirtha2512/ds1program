39be2e7a74c2d974ff7455d14aa5dcf1f666347d
Tirtha2512 <tirthagohel159@gmail.com>
2026-07-07 07:41:47 +0530

    Add files via upload

---
+++ pnz.py
@@ -0,0 +1,7 @@
+enter_no=int(input("enter number:"))
+if enter_no>0:
+    print("this is positive no.",enter_no)
+elif enter_no<0:
+    print("this is nagetive no.",enter_no)
+else:
+    print("this is zero:",enter_no)
