203d4dc9f052370b26dfa5e029613b77a0dbbe51
Tirtha2512 <tirthagohel159@gmail.com>
2026-07-07 07:41:24 +0530

    Add files via upload

---
+++ oe.py
@@ -0,0 +1,8 @@
+start_no=int(input("enter start no:"))
+stop_no=int(input("enter stop no:"))
+for i in range(start_no,stop_no):
+    if i%2==0:
+        print(i,"\t EVEN NUMBER")
+    else :
+        print("\t\t",i, "ODD NUMBER")
+      
