83bf5fc8519c73f467c28b21d303f26e033e8622
Tirtha2512 <tirthagohel159@gmail.com>
2026-07-10 09:19:30 +0530

    Add files via upload

---
+++ arr1.py
@@ -0,0 +1,17 @@
+import sys
+import numpy as np
+arr1=np.array([10,20,30,40,50])
+print(arr1)
+print("----------------------")
+arr2=np.array([90,78,20,69,89])
+print("addition",arr1+arr2)
+print("subtraction",arr1-arr2)
+print("multiplication",arr1*arr2)
+print("division",arr1/arr2)
+print("----------------------")
+print("addition",arr1+15)
+print("subtraction",arr1-15)
+print("multiplication",arr1*15)
+print("division",arr1/15)
+print("module",arr1%15)
+
