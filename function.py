2b3876ca2ec4b37b2e72231b59981a699997c8b5
Tirtha2512 <tirthagohel159@gmail.com>
2026-07-14 08:03:18 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,20 @@
+#universal function
+import sys
+import numpy as np
+arr=np.array([1,4,6,16,20])
+
+print("sqrt:",np.sqrt(arr))
+print("square:",np.square(arr))
+print("sin:",np.sin(arr))
+print("cos:",np.cos(arr))
+
+#Aggregrate function
+import sys
+import numpy as np
+arr1=np.array([10,80,20,60,50])
+
+print("sum:",np.sum(arr1))
+print("mean:",np.mean(arr1))
+print("max:",np.max(arr1))
+print("min:",np.min(arr1))
+print("std:",np.std(arr1))
