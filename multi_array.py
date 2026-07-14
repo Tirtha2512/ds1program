e0b0ee5210c1abe82de2a96604fc45de26b7c88d
Tirtha2512 <tirthagohel159@gmail.com>
2026-07-14 08:11:09 +0530

    Add files via upload

---
+++ multi_array.py
@@ -0,0 +1,15 @@
+import sys
+import numpy as np
+
+arr=np.array([[10,20,30,40,50],[60,70,80,90,100]])
+print(arr)
+print(arr[0,1])
+print(arr[0,2])
+print(arr[0,3])
+print(arr[0,4])
+
+
+print(arr[1,0])
+print(arr[1,1])
+print(arr[1,3])
+print(arr[1,4])
