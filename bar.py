5a128c290d63617fbfb12dadc0c48779585409fb
Tirtha2512 <tirthagohel159@gmail.com>
2026-08-04 08:37:06 +0530

    Add files via upload

--- bar.py
+++ bar.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 df=pd.read_csv(r"D:\bscitE2_14\ds\sales_data.csv")
-plt.bar(df["Month"],df["Sales"],color="#ACDF54")
+plt.bar(df["Month"],df["Sales"],color="#DF2354")
 plt.title("Monthly Sales")
 plt.xlabel("Month")
 plt.ylabel("Sales")
